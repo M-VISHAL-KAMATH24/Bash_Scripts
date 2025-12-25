4a0c8e07fed50028055a0fa4c0cac60093c171eb
Vishal <22k20.vishal@sjec.ac.in>
2025-12-25 16:39:34 +0530

    initial frontend folder for chatapp

---
+++ chat_app/chatapp_frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h4 className='text-5xl'>
+        welcome to the website
+      </h4>
+    </div>
+  )
+}
+
+export default App
