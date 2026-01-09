8685198e80e4606e45d23ee17b7f65e19bd06f57
Vishal <22k20.vishal@sjec.ac.in>
2026-01-09 22:04:52 +0530

    signin conmtroller

--- nineleaps_intern/backend_day2/controllers/auth.controller.js
+++ nineleaps_intern/backend_day2/controllers/auth.controller.js
@@ -45,6 +45,38 @@ try{
 
 export const signIn=async(req,res,next)=>{
 
+
+    try{
+        const {email,password}=req.body;
+        const user=await User.findOne({email});
+        if(!user){
+            const error=new Error('user not found');
+            error.statusCode=404;
+            throw error;
+        }
+        const isPasswordValid=await bcrypt.compare(password,user.password);
+        if(!isPasswordValid){
+            const error=new Error('invalid user');
+            error.statusCode=401;
+            throw error;
+        }
+         const token = jwt.sign(
+  { userId: user._id },
+  process.env.JWT_SECRET,
+  { expiresIn: process.env.JWT_EXPIRES_IN });
+   res.status(200).json({
+        sucess:true,
+        message:'user signed in successfully',
+        data:{
+            token,
+            user
+        }
+    })
+
+
+    }catch(error){
+        next(error);
+    }
 }
 
 
